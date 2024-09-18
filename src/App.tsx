2a1496dbd9a286fba5053a3ca7e79424f0ee67e2
Lucas Severino <lucas.sseverino@gmail.com>
2024-09-17 22:39:11 -0300

    install immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import Button from "./components/Button/Button";
-import Like from "./components/Like/like";
+import { useState } from "react";
+import Like from "./components/Like/Like";
+import produce from "immer";
 
 const App = () => {
 
+  const [bugs, setBugs] = useState([
+    {
+      id: 1,
+      title: 'Bug 1',
+      fixed: false
+    },
+    {
+      id: 2,
+      title: 'Bug 2',
+      fixed: false
+    }
+  ]);
+
+  const handleClick = () => {
+    setBugs(
+      produce(draft => {
+        const bug = draft.find(bug => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  }
 
   return (
     <div>
-      <Like onClick={() => console.log("You just clicked Like!")} />
+      {/* <Like onClick={() => console.log("You just clicked Like!")} /> */}
+        {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
+        <button onClick={handleClick}>Update bug status</button>
     </div>
   );
 }
