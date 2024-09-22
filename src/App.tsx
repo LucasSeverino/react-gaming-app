2145625586c8b5443c9e59b5998d4d05f52e88a0
Lucas Severino <lucas.sseverino@gmail.com>
2024-09-22 12:34:06 -0300

    feat: create expense-tracker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 import { useState } from "react";
-import Like from "./components/Like/Like";
-import produce from "immer";
-import ExpandableText from "./components/ExpandableText/ExpandableText";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
 
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 20, category: "Groceries" },
+    { id: 3, description: "ccc", amount: 30, category: "Entertainment" },
+    { id: 4, description: "ddd", amount: 40, category: "Utilities" },
+  ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
-    <ExpandableText maxChars={10}>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam accusantium itaque velit vel. Consequatur
-      quia veritatis vero, libero vitae exercitationem, aspernatur nostrum doloremque recusandae maiores voluptatum repellat alias
-      qui? Tempore.
-    </ExpandableText>
+    <div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        ></ExpenseForm>
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
+    </div>
   );
-}
+};
 
 export default App;
