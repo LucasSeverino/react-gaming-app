4d0261e6eb120d849ebe392221a5cf62332394c0
Lucas Severino <lucas.sseverino@gmail.com>
2024-09-22 15:08:38 -0300

    create custom service and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CanceledError } from "./services/api-client";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
+
+const API_URL = "https://jsonplaceholder.typicode.com";
 
 const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 20, category: "Groceries" },
-    { id: 3, description: "ccc", amount: 30, category: "Entertainment" },
-    { id: 4, description: "ddd", amount: 40, category: "Utilities" },
-  ]);
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
-    <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        ></ExpenseForm>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        ></ExpenseFilter>
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      ></ExpenseList>
-    </div>
+    <>
+      {isLoading && <div className="spinner-border"></div>}
+      {error && <p className="text-danger">{error}</p>}
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
